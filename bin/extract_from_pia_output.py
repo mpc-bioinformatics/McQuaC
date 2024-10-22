2b7c61aa5e552acb9ec7b1bd1dcf70c3241d70f7
julianu <julian.uszkoreit@rub.de>
2024-10-22 08:10:46 +0000

    storing PIA QC values in HDF5 and small refactoring

--- bin/extract_from_pia_output.py
+++ bin/extract_from_pia_output.py
@@ -3,9 +3,6 @@
 import pandas as pd
 import numpy as np
 import argparse
-import zipfile
-import base64
-import os
 import io
 import h5py
 from typing import List, Any
@@ -17,6 +14,7 @@ def argparse_setup():
     parser.add_argument("--pia_proteins", help="Proteins.mzTab from PIA output")
     parser.add_argument("--pia_PSMs", help="PSM.mzTab from PIA output")
     parser.add_argument("--out_hdf5", help="Output HDF5 with statistics")
+    parser.add_argument("--store_all_infos", help="Store all PSMs, peptides and proteins in the HDF5 file", default=False, type=bool)
 
     return parser.parse_args()
 
@@ -42,8 +40,7 @@ def add_entry_to_hdf5(
     f[key].attrs["unit_name"] = unit_name
 
 
-
-def add_table_in_hdf5(
+def add_table_to_hdf5(
     f, qc_acc: str, qc_short_name: str, qc_name: str, qc_description: str, 
     column_names: List[str], column_data: List[List[Any]], column_types: List[str]
     ): 
@@ -70,34 +67,15 @@ def run_pia_extraction():
 
     # Open HDF5 file in write mode
     with h5py.File(args.out_hdf5, 'w') as out_h5:
-        count_nr_Proteins(args.pia_proteins, out_h5)
-        count_nr_filtered_peptides(args.pia_peptides, out_h5)
-        read_mzTab(args.pia_PSMs, out_h5)
-
-        # Save the zip files also in HDF5
-        # zipfile.ZipFile("pia_extractions.zip", mode="w", compresslevel=9).write(args.pia_peptides,
-        #                                                                 compress_type=zipfile.ZIP_DEFLATED,
-        #                                                                 compresslevel=9,
-        #                                                                 arcname=args.pia_peptides.split(os.sep)[-1])
-        # zipfile.ZipFile("pia_extractions.zip", mode="w", compresslevel=9).write(args.pia_PSMs,
-        #                                                                 compress_type=zipfile.ZIP_DEFLATED,
-        #                                                                 compresslevel=9,
-        #                                                                 arcname=args.pia_PSMs.split(os.sep)[-1])
-        # zipfile.ZipFile("pia_extractions.zip", mode="w", compresslevel=9).write(args.pia_proteins,
-        #                                                                 compress_type=zipfile.ZIP_DEFLATED,
-        #                                                                 compresslevel=9,
-        #                                                                 arcname=args.pia_proteins.split(os.sep)[-1])
-        # with open("pia_extractions.zip", "rb") as pia_b:
-        #     pia_str_bs64 = base64.b64encode(pia_b.read()).decode("utf-8")
-        # out_h5.create_dataset("pia_output.zip", data=pia_str_bs64)
-        # out_h5["pia_output.zip"].attrs["unit"] = "zip File"
-        # out_h5["pia_output.zip"].attrs["Description"] = "A zip container containing the used PSM-mzTAB, Peptide-CSV and Proteins-mzTAB file from the PIA-Output. This zip file was base64 encoded to be represented as a string"
+        parse_protein_infos(args.pia_proteins, out_h5)
+        parse_peptide_infos(args.pia_peptides, out_h5)
+        parse_psm_infos(args.pia_PSMs, out_h5, args.store_all_infos)
 
 
-def count_nr_filtered_peptides(pia_peptide, out_hdf5) -> int:
+def parse_peptide_infos(pia_peptide_csv: str, out_hdf5: h5py.File) -> int:
     # needs the PIA peptides.csv output file and counts PEPTIDE occurences
     # peptides are already filtered for FDR and decoys by the PIA export
-    with open(pia_peptide) as f:
+    with open(pia_peptide_csv) as f:
         text = "\n".join([line for line in f if line.startswith("PEPTIDE") or line.startswith("COLS_PEPTIDE") or line.startswith('"COLS_PEPTIDE"')])
 
     if text:
@@ -112,15 +90,15 @@ def count_nr_filtered_peptides(pia_peptide, out_hdf5) -> int:
                       "UO:0000189", "count unit")
 
 
-def count_nr_Proteins(pia_prots, out_hdf5):
+def parse_protein_infos(pia_proteins_mztab: str, out_hdf5: h5py.File):
     # read in proteins
-    with open(pia_prots) as f:
+    with open(pia_proteins_mztab) as f:
         text = "\n".join([line for line in f if line.startswith("PRH") or line.startswith("PRT")])
 
     if text:
         protein_df = pd.read_csv(io.StringIO(text), sep="\t")
 
-        # proteins are already FDR and decoy filtered by json parameters
+        # proteins are already FDR and decoy filtered by PIA settings
         number_proteins = protein_df.shape[0]
 
         ungrouped_proteins = list()
@@ -142,50 +120,54 @@ def count_nr_Proteins(pia_prots, out_hdf5):
                       "UO:0000189", "count unit")
 
 
-def read_mzTab(pia_mzTAB, out_hdf5):
+def parse_psm_infos(pia_psm_mztab: str, out_hdf5: h5py.File, store_all_infos: bool = False):
     # get header for PSM FDR Score
-    with open(pia_mzTAB) as f:
+    with open(pia_psm_mztab) as f:
         text = "\n".join([line for line in f if line.startswith("MTD")])
 
     if text: 
         mzTab_header = pd.read_csv(io.StringIO(text), sep="\t", header=None)
-        psm_score_header = mzTab_header[mzTab_header[2].str.contains('MS:1002355')].iloc[-1][1]
+        psm_score_header = mzTab_header[mzTab_header[2].str.contains('MS:1002355')].iloc[-1][1] # this is the PSM-level FDR score
         psm_score_header = psm_score_header[4:]     # remove "psm_" at beginning
 
         ## read in PSMs
-        with open(pia_mzTAB) as f:
+        with open(pia_psm_mztab) as f:
             text = "\n".join([line for line in f if line.startswith("PS")])
 
         psm_df = pd.read_csv(io.StringIO(text), sep="\t")
 
-        # remove decoys and filter PSM columns
+        # remove decoys and filter for important PSM columns
         psm_df = psm_df.loc[psm_df['opt_global_cv_MS:1002217_decoy_peptide'] == 0]
-        psm_df = psm_df.loc[:,["PSM_ID", "sequence", "accession", "unique", "retention_time", "charge", "opt_global_missed_cleavages", "modifications", "retention_time", "exp_mass_to_charge", "calc_mass_to_charge", "spectra_ref", psm_score_header]]
+        psm_df = psm_df.loc[:,["PSM_ID", "sequence", "accession", "unique", "retention_time", "charge", "opt_global_missed_cleavages", "modifications", "exp_mass_to_charge", "calc_mass_to_charge", "spectra_ref", psm_score_header]]
         
         # group the accessions
         gbseries = psm_df.groupby(by=['PSM_ID'])['accession']
         psm_df['accession'] = psm_df["PSM_ID"].map(gbseries.apply(",".join))
         psm_df = psm_df.drop_duplicates()
 
+        unfiltered_psms = psm_df.copy()
+        unfiltered_psms.rename(columns={psm_score_header: 'psm_level_fdrscore'}, inplace=True)
+
         # filter FDR <= 0.01    # TODO: parameterize?
         psm_df = psm_df.loc[psm_df[psm_score_header] <= 0.01]
 
-        ### Calculate ppm error
+        # Calculate ppm error
         exp_calc_diff = psm_df["exp_mass_to_charge"] - psm_df["calc_mass_to_charge"]
         exp_calc_diff_removed_isotopes = exp_calc_diff - (exp_calc_diff.round()).astype(int) # Remove Isotopes, since calc_mass expects none
         ppm_error_df = (exp_calc_diff_removed_isotopes * 1000000) / psm_df["calc_mass_to_charge"]
         ppm_error = ppm_error_df.to_list()
 
+        # now get all counts as QC metrics
         PSM_count = psm_df.shape[0]
         
         nr_psms = psm_df.shape[0] if psm_df.shape[0] != 0 else 1
-        charge_counts_above5 = psm_df[psm_df['charge'] > 5]['PSM_ID'].count() / nr_psms
-        charge_counts_1      = psm_df[psm_df['charge'] == 1]['PSM_ID'].count() / nr_psms
-        charge_counts_2      = psm_df[psm_df['charge'] == 2]['PSM_ID'].count() / nr_psms
-        charge_counts_3      = psm_df[psm_df['charge'] == 3]['PSM_ID'].count() / nr_psms
-        charge_counts_4      = psm_df[psm_df['charge'] == 4]['PSM_ID'].count() / nr_psms
-        charge_counts_5      = psm_df[psm_df['charge'] == 5]['PSM_ID'].count() / nr_psms
-        charge_fractions = [[charge_counts_1], [charge_counts_2], [charge_counts_3], [charge_counts_4], [charge_counts_5], [charge_counts_above5]]
+        charge_fraction_1    = psm_df[psm_df['charge'] == 1]['PSM_ID'].count() / nr_psms
+        charge_fraction_2    = psm_df[psm_df['charge'] == 2]['PSM_ID'].count() / nr_psms
+        charge_fraction_3    = psm_df[psm_df['charge'] == 3]['PSM_ID'].count() / nr_psms
+        charge_fraction_4    = psm_df[psm_df['charge'] == 4]['PSM_ID'].count() / nr_psms
+        charge_fraction_5    = psm_df[psm_df['charge'] == 5]['PSM_ID'].count() / nr_psms
+        charge_fraction_more = psm_df[psm_df['charge'] > 5]['PSM_ID'].count() / nr_psms
+        charge_fractions = [[charge_fraction_1], [charge_fraction_2], [charge_fraction_3], [charge_fraction_4], [charge_fraction_5], [charge_fraction_more]]
 
         miss_count_0    = psm_df[psm_df['opt_global_missed_cleavages'] == 0]['PSM_ID'].count()
         miss_count_1    = psm_df[psm_df['opt_global_missed_cleavages'] == 1]['PSM_ID'].count()
@@ -205,12 +187,12 @@ def read_mzTab(pia_mzTAB, out_hdf5):
                       PSM_count, (1,), "int32", 
                       "UO:0000189", "count unit")
     
-    add_table_in_hdf5(out_hdf5,
+    add_table_to_hdf5(out_hdf5,
                       "Local:09", "psm_charge_fractions", "TODO", "TODO", 
                       ["1", "2", "3", "4", "5", "6 or more"], charge_fractions, ["float64", "float64", "float64", "float64", "float64", "float64"]
     )
 
-    add_table_in_hdf5(out_hdf5,
+    add_table_to_hdf5(out_hdf5,
                       "MS:4000180", "psm_missed_counts", "table of missed cleavage counts", "The number of identified peptides with corresponding number of missed cleavages after user-defined acceptance criteria are applied. The number of missed cleavages per peptide is given in the 'number of missed cleavages' column, the respective count of such peptides identified in the 'Number of Occurrences' column. The highest 'missed cleavages' row is to be interpreted as that number of missed cleavages or higher.", 
                       ["0", "1", "2", "3", "4 or more"], miss_counts, ["uint32", "uint32", "uint32", "uint32", "uint32"]
     )
@@ -219,6 +201,19 @@ def read_mzTab(pia_mzTAB, out_hdf5):
                       "LOCAL:05", "filtered_psms_ppm_error", "deviations in PPM for each PSM", "TODO", 
                       ppm_error, (len(ppm_error),), "float64", 
                       "UO:0000169", "parts per million")
+    
+    if store_all_infos and (unfiltered_psms is not None):
+        # TODO: fix in PIA
+        unfiltered_psms["modifications"] = unfiltered_psms["modifications"].astype("str")
+
+        column_names = unfiltered_psms.columns.tolist()
+        psms_data = [unfiltered_psms[x].astype("str") for x in column_names]
+        column_types = unfiltered_psms.dtypes.astype("str").tolist()
+
+        add_table_to_hdf5(out_hdf5,
+                        "identified_psms_table", "identified_psms_table", "table of all unfiltered identified PSMs", "All identified PSM as reported by PIA", 
+                        column_names, psms_data, column_types
+        )
 
 if __name__ == "__main__":
     run_pia_extraction()
